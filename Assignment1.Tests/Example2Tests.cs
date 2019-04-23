d0457eacaeff3dfe7f1bf56eb624d768fd2135c8
Andreas Leonhard <aul@pentia.dk>
2019-04-23 14:31:42 +0200

    minor changes

---
+++ Assignment1.Tests/Example2Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment1;
+using Xunit;
+namespace Assignment1.Tests
+{
+	public class Example2Tests
+	{
+		static TimeSpan timeTest1 = new TimeSpan(0, 00, 00);
+
+		[Fact]
+		public void TicketIsASaverTicket()
+		{
+			bool result = Example2.CheckSaverTicket(time);
+
+			Assert.InRange(TimeSpan timeTest1);
+		}
+	}
+}
---
+++ Assignment1/Example2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DateTime = System.DateTime;
+
+namespace Assignment1
+{
+	public class Example2
+	{
+
+		public static Boolean CheckSaverTicket(TimeSpan time)
+		{
+			TimeSpan saverTicketStart930 = new TimeSpan(9, 30, 00);
+			TimeSpan saverTicketEnd1600 = new TimeSpan(16, 00, 00);
+			TimeSpan saverTicketStart1930 = new TimeSpan(19, 30, 00);
+			TimeSpan saverTicketEnd2359 = new TimeSpan(23, 59, 59);
+
+			if (time > saverTicketStart930 && time < saverTicketEnd1600)
+			{
+				return true;
+			}
+
+			if (time > saverTicketStart1930 && time < saverTicketEnd2359)
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
