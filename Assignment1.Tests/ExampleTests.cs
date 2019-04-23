3213f1246c76cb03a229c0fdd61d3363e705b6fa
Andreas Leonhard <aul@pentia.dk>
2019-04-23 11:49:51 +0200

    Added code, tests and code coverage

---
+++ Assignment1.Tests/ExampleTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment1;
+using Xunit;
+
+namespace Assignment1.Tests
+{
+	public class ExampleTests
+	{
+
+		[Fact]
+		public void ArgInputDatasetA()
+		{
+			string[] arg = {};
+
+			// Arrange
+			string expected = "No numbers";
+
+			// Act
+			string actual = Example.GetStringResult(arg);
+
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void ArgInputDatasetB()
+		{
+			string[] arg = { "17" };
+			// Arrange
+			string expected = "Minimum = " + "17" + "; maximum = " + "17"; ;
+
+			// Act
+			string actual = Example.GetStringResult(arg);
+
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void ArgInputDatasetC()
+		{
+			string[] arg = { "27","29" };
+			// Arrange
+			string expected = "Minimum = " + "27" + "; maximum = " + "29"; ;
+
+			// Act
+			string actual = Example.GetStringResult(arg);
+
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void ArgInputDatasetD()
+		{
+			string[] arg = { "39", "37" };
+			// Arrange
+			string expected = "Minimum = " + "37" + "; maximum = " + "39"; ;
+
+			// Act
+			string actual = Example.GetStringResult(arg);
+
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void ArgInputDatasetE()
+		{
+			string[] arg = { "49", "47", "48" };
+			// Arrange
+			string expected = "Minimum = " + "47" + "; maximum = " + "49"; ;
+
+			// Act
+			string actual = Example.GetStringResult(arg);
+
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+	}
+
+	// string[] arg = new string[] { "A", "B", "C", "D", "E" };
+}
---
+++ Assignment1/Example.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Example
+    {
+	    public static void Main(String[] args)
+	    {
+		    string result = GetStringResult(args);
+		    Console.Write(result);
+	    }
+
+	    public static string GetStringResult(String[] args)
+	    {
+		    int mi, ma;
+		    if (args.Length == 0) /* 1 */
+		    {			    
+			    return "No numbers";
+		    }
+		    else
+		    {
+			    mi = ma = int.Parse(args[0]);
+			    for (int i = 1; i < args.Length; i++) /* 2 */
+			    {
+				    int obs = int.Parse(args[i]);
+				    if (obs > ma) ma = obs; /* 3 */
+				    else if (mi < obs) mi = obs; /* 4 */
+			    }
+			    return "Minimum = " + mi + "; maximum = " + ma;
+		    }
+	    }
+	}
+}
